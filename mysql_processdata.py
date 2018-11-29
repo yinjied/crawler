ac6b39f2f3dab60a2e0417d0a02a8c882eb48092
yinjied <yinjied@126.com>
2018-11-29 09:53:21 +0800

    hhh

--- mysql_processdata.py
+++ mysql_processdata.py
@@ -1,32 +1,21 @@
 import pymysql
-host="localhost"
+host="139.198.19.54"
 user = "crawler"
 passwd = "crawler"
 dbname = "test"
 
-db = pymysql.connect(host,user,passwd,dbname )
-cursor = db.cursor()
 
-sql = "INSERT INTO district (district, district_url) VALUES (%s, %s)"
-sql = "SELECT * FROM district"
-cursor.execute(sql)
-ret = cursor.fetchone()
-ret1 = cursor.fetchone()
-print(type(ret))
-print(ret)
-print(ret1)
-db.close()
-print('你好')
-
-
-def insert_district(district_dict):
-    if (len(district_dict) == 0):
-        return 1
-    conn = pymysql.connect(host,user,passwd,dbname )
+def func():
+    conn = pymysql.connect(host,user,passwd,dbname,charset='utf8' )
     cursor = conn.cursor()
-    for item in district_dict:
-        sql = "INSERT INTO district (district, district_url) VALUES (%s, %s)"
-        cursor.execute(sql, (item, district_dict[item]))
+
+    sql = "SELECT distinct xiaoqu_name, xiaoqu_url FROM apartment"
+    cursor.execute(sql)
+    xiaoqu_tuple = cursor.fetchall()
+    for item in xiaoqu_tuple:
+        sql = "insert into xiaoqu (xiaoqu_name, xiaoqu_url) values (%s, %s)"
+        cursor.execute(sql,item)
     conn.commit()
     conn.close()
-    return 0
+
+
